fef19f87539a2e72eb21ced511403d0ba7b48def
siddht4 <dhar.75.ap@gmail.com>
2019-11-05 15:43:08 +0530

    Update moku.c

--- moku.c
+++ moku.c
@@ -1,20 +1,48 @@
 #include <Python.h>
 #include <stdio.h>
 
-unsigned char c_sum(char num_chars[]) {
-    
-    return num_chars[0];
-}
 
 
 static PyObject *foo_bar(PyObject *self, PyObject *args)
 {
-const char *s;
-Py_ssize_t size;
-    if (!PyArg_ParseTuple(args, "s", &s)) {
-        return NULL;
-    }
-return PyArg_ParseTuple(args, c_sum(args), &s,size);
+PyObject *bufobj; 
+    Py_buffer view; 
+    double result; 
+    /* Get the passed Python object */
+    if (!PyArg_ParseTuple(args, "O", &bufobj)) 
+    { 
+        return NULL; 
+    } 
+  
+    /* Attempt to extract buffer information from it */
+  
+    if (PyObject_GetBuffer(bufobj, &view, 
+                           PyBUF_ANY_CONTIGUOUS | PyBUF_FORMAT) == -1) 
+    { 
+        return NULL; 
+    } 
+  
+    if (view.ndim != 1) 
+    { 
+        PyErr_SetString(PyExc_TypeError, "Expected a 1-dimensional array"); 
+        PyBuffer_Release(&view); 
+        return NULL; 
+    } 
+  
+    /* Check the type of items in the array */
+    if (strcmp(view.format, "d") != 0) 
+    { 
+        PyErr_SetString(PyExc_TypeError, "Expected an array of doubles"); 
+        PyBuffer_Release(&view); 
+        return NULL; 
+    } 
+  
+    /* Pass the raw buffer and size to the C function */
+    result = avg(view.buf, view.shape[0]); 
+  
+    /* Indicate we're done working with the buffer */
+    PyBuffer_Release(&view); 
+    return Py_BuildValue("d", result); 
 }
 
 
