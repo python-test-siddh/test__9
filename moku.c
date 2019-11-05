ae2287bbbdb012646432f0943967b8aaa6594d33
siddht4 <dhar.75.ap@gmail.com>
2019-11-05 14:48:58 +0530

    Update moku.c

--- moku.c
+++ moku.c
@@ -1,13 +1,20 @@
 #include <Python.h>
 #include <stdio.h>
 
+unsigned char c_sum(char num_chars[]) {
+    
+    return num_chars[];
+}
+
+
 static PyObject *foo_bar(PyObject *self, PyObject *args)
 {
 const char *s;
 Py_ssize_t size;
-if (!PyArg_ParseTuple(args, ""))
-return NULL;
-return PyArg_ParseTuple(args);
+    if (!PyArg_ParseTuple(args, "s", &s)) {
+        return NULL;
+    }
+return PyArg_ParseTuple(args, c_sum(args), &s);
 }
 
 
