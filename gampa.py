8b8c7d72022bb0a14d10303b6f0c319c7dd8cfbf
siddht4 <dhar.75.ap@gmail.com>
2019-11-06 10:56:20 +0530

    Update gampa.py

--- gampa.py
+++ gampa.py
@@ -4,7 +4,12 @@ for _a in dir(moku):
 print(moku)
 print(moku.moku)
 
-_st='yellow'
+_st='y'
 _arr=bytes(_st,'utf-8')
 print(moku.moku(_arr))
+
+
+
+print(type(moku.moku(_arr)))
+print(len(moku.moku(_arr)))
 
